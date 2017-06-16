b592c69b6eb29efa4de8fbbba61dcaa133d8c47c
Necromatron <komarov.99@yandex.ru>
2017-06-16 12:15:26 +0300

    First part of Resource Manager

---
+++ MASengine/MeshClass.h
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: meshClass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MESHCLASS_H_
+#define _MESHCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <fstream>
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include <d3d10.h>
+#include <d3dx10.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class MeshClass
+{
+private:
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+		D3DXVECTOR2 texture;
+		D3DXVECTOR3 normal;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+public:
+	MeshClass();
+	MeshClass(const MeshClass&);
+	~MeshClass();
+
+	bool Initialize(ID3D10Device* device, const std::string& filename);
+	void Shutdown();
+	void Render(ID3D10Device* device);
+
+	int GetIndexCount();
+	void getBox(float& xSize, float& ySize, float& zSize);
+private:
+	bool InitializeBuffers(ID3D10Device* device);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D10Device* device);
+
+	bool LoadModel(const std::string& filename);
+	void ReleaseModel();
+
+private:
+	ID3D10Buffer *m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+	ModelType* m_model;
+};
+
+#endif
---
+++ MASengine/TextureManagerClass.h
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: TextureManagerClass.h
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _TEXTUREMANAGERCLASS_H_
+#define _TEXTUREMANAGERCLASS_H_
+
+//////////////
+// INCLUDES //
+//////////////
+#include <filesystem>
+#include <string>
+#include <map>
+#include <codecvt>
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "TextureClass.h"
+#include "ModManagerClass.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: TextureManagerClass
+////////////////////////////////////////////////////////////////////////////////
+
+class TextureManagerClass
+{
+public:
+
+	TextureManagerClass();
+	TextureManagerClass(const TextureManagerClass&);
+	~TextureManagerClass();
+
+	void Shutdown();
+
+	bool addTexture(ID3D10Device* device, const std::string& filename);
+	ID3D10ShaderResourceView* getTexture(const std::string& filename);
+
+private:
+	std::map<long long, TextureClass*> m_textures;
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> m_converter;
+};
+
+#endif
